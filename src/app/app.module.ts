b706828f31baf3d82f5914311bc613cfce4edbdf
bubu2323 <carolinatronci2021@gmail.com>
2023-10-13 17:31:11 +0200

    add header component, add style for navbar and add logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './shared/header/header/header.component';
+// every component needs do declared here
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
