4a66a8b3e0cdec230c3b2baa8a0a9a107a440aac
bubu2323 <carolinatronci2021@gmail.com>
2024-02-09 12:00:13 +0100

    add routing and create home,movies,genres and series components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {MoviesComponent} from "./pages/movies/movies.component";
+import {SeriesComponent} from "./pages/series/series.component";
+import {GenresComponent} from "./pages/genres/genres.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'movies', component: MoviesComponent},
+  { path: 'series', component: SeriesComponent},
+  { path: 'genres', component: GenresComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header/header.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -11,6 +11,12 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { SeriesComponent } from './pages/series/series.component';
+import { GenresComponent } from './pages/genres/genres.component';
+import { SliderComponent } from './shared/components/slider/slider.component';
 
 
 // every component needs do declared here
@@ -18,6 +24,12 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    MoviesComponent,
+    SeriesComponent,
+    GenresComponent,
+    SliderComponent,
   ],
   imports: [
     BrowserModule,
